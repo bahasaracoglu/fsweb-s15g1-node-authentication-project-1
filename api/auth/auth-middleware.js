d8070fd1d038f913c0e3b83a87c865f18fecd2dc
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-29 21:28:52 +0300

    aut-mw (sınırlı hariç)

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const userModel = require("../users/users-model");
+const bcrypt = require("bcryptjs");
+
 /*
   Kullanıcının sunucuda kayıtlı bir oturumu yoksa
 
@@ -6,9 +9,7 @@
     "message": "Geçemezsiniz!"
   }
 */
-function sinirli() {
-
-}
+function sinirli() {}
 
 /*
   req.body de verilen username halihazırda veritabanında varsa
@@ -18,8 +19,18 @@ function sinirli() {
     "message": "Username kullaniliyor"
   }
 */
-function usernameBostami() {
-
+async function usernameBostami(req, res, next) {
+  try {
+    let { username } = req.body;
+    const isExist = await userModel.goreBul({ username: username });
+    if (isExist && isExist.length > 0) {
+      res.status(442).json({ messsage: "Username kullaniliyor" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -30,8 +41,19 @@ function usernameBostami() {
     "message": "Geçersiz kriter"
   }
 */
-function usernameVarmi() {
+async function usernameVarmi() {
+  try {
+    let { username } = req.body;
+    const isExist = await userModel.goreBul({ username: username });
 
+    if (isExist && isExist.length > 0) {
+      res.status(401).json({ messsage: "Geçersiz kriter" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -42,8 +64,41 @@ function usernameVarmi() {
     "message": "Şifre 3 karakterden fazla olmalı"
   }
 */
-function sifreGecerlimi() {
-
+function sifreGecerlimi(res, req, next) {
+  try {
+    let { password } = req.body;
+    if (!password || password.length < 3) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
+
+/*
+Payload'da iletilen bilgilere ait doğrulama
+*/
+
+function checkPayload(req, res, next) {
+  try {
+    let { username, password } = req.body;
+    if (!username || !password) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  checkPayload,
+  sifreGecerlimi,
+  usernameVarmi,
+  usernameBostami,
+};
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
